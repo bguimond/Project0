9caf89e75eaf66b0aeab8e0957ecefc70670701c
bguimond <55721907+bguimond@users.noreply.github.com>
2021-02-12 20:17:31 -0600

    Update p0.rkt

--- p0.rkt
+++ p0.rkt
@@ -14,15 +14,27 @@
 
 ; Compute the truth value of the proposition "x --> y" where x and y are booleans
 (define (implies-value x y)
-  'todo)
+  (if (equal? x y) ; (T --> T = T) and (F --> F = T)
+  true
+  (if (and (equal? x true) (equal? y false))
+        false ;T --> F = F
+        true  ;F --> T = T 
+  )))
 
-; Compute the median of three integers 
+; Compute the median of three integers
+; since there are only three then we never need to divide, simple comparisons will suffice  
 (define (median-of-three x y z)
-  'todo)
+  (if (and (< x y) (< y z)) ;if already sorted
+      y
+      (if (and (< y x) (< x z)) ;if not sorted
+      x
+      z
+  )))
+
 
 ; Compute the distance between two (x,y) pairs of integers (as an inexact number, like 2.0)
 (define (point-distance x0 y0 x1 y1)
-  'todo)
+  (sqrt( + (expt (- x1 x0) 2) (expt (- y1 y0) 2)))) ;simply distance point formula
 
 
 ; Takes a list, lst (e.g., '(1 2 3 #f 4 #t))
@@ -32,11 +44,10 @@
 (define (valid-list-prefix lst valid?)
   'todo)
 
+
 ; Takes a list, lst (e.g., '(4 5 #t 'a 'b 3 'c))
 ; and a predicate, pred (e.g., symbol?)
 ; and yields a list of sublists (in the original list) for nonempty sequences of elements not matching 
 ; the predicate; omit any empty lists (e.g., '((4 5 #t) (3)))
 (define (explode-list-by-predicate lst pred)
   'todo)
-
-
